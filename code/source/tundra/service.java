e5ea2e13db7009e2ee82ced99aa84994cdca0658
Lachlan Dowding <lachlan@permafro.st>
2014-08-14 09:05:24 +1000

    Change tundra.service:invoke to return invocation duration when $mode is synchronous; closes #75

--- code/source/tundra/service.java
+++ code/source/tundra/service.java
@@ -1,8 +1,8 @@
 package tundra;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2014-08-09 19:02:53 EST
-// -----( ON-HOST: 172.16.189.141
+// -----( CREATED: 2014-08-14 08:54:28.016
+// -----( ON-HOST: -
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -40,14 +40,14 @@ public final class service
 		// [o] field:0:required $duration.stdev
 		// [o] field:0:required $message
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String service = IDataUtil.getString(cursor, "$service");
 		  IData scope = IDataUtil.getIData(cursor, "$pipeline");
 		  int count = Integer.parseInt(IDataUtil.getString(cursor, "$count"));
-		
+
 		  IncrementalNormalDistributionEstimator estimator = benchmark(service, scope == null? pipeline : scope, count);
-		
+
 		  IDataUtil.put(cursor, "$duration.average", tundra.duration.format("" + estimator.mean(), "milliseconds", "xml"));
 		  IDataUtil.put(cursor, "$duration.stdev", tundra.duration.format("" + estimator.standardDeviation(), "milliseconds", "xml"));
 		  IDataUtil.put(cursor, "$message", service + " benchmark results: " + estimator.toString());
@@ -56,7 +56,7 @@ public final class service
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -71,7 +71,7 @@ public final class service
 		// [o] field:0:required $callers
 		// [o] field:0:required $caller
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String[] stack = callstack();
 		  String callers = tundra.list.object.join(stack, " \u2192 ");
@@ -83,7 +83,7 @@ public final class service
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -100,23 +100,23 @@ public final class service
 		// [i] record:0:optional $pipeline
 		// [o] record:0:optional $pipeline
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String $service = IDataUtil.getString(cursor, "$service");
 		  String $catch = IDataUtil.getString(cursor, "$catch");
 		  String $finally = IDataUtil.getString(cursor, "$finally");
 		  IData scope = IDataUtil.getIData(cursor, "$pipeline");
 		  boolean scoped = scope != null;
-		
+
 		  scope = ensure($service, scoped ? scope : pipeline, $catch, $finally);
-		
+
 		  if (scoped) IDataUtil.put(cursor, "$pipeline", scope);
 		} finally {
 		  cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -129,7 +129,7 @@ public final class service
 		// @sigtype java 3.5
 		// [o] field:0:required $initiator?
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  IDataUtil.put(cursor, "$initiator?", "" + initiator());
 		} finally {
@@ -137,7 +137,7 @@ public final class service
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -153,28 +153,33 @@ public final class service
 		// [i] field:0:optional $mode {&quot;synchronous&quot;,&quot;asynchronous&quot;}
 		// [o] record:0:optional $pipeline
 		// [o] object:0:optional $thread
+		// [o] field:0:optional $duration
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String service = IDataUtil.getString(cursor, "$service");
 		  IData scope = IDataUtil.getIData(cursor, "$pipeline");
 		  String mode = IDataUtil.getString(cursor, "$mode");
-		
+
 		  if (mode == null) mode = "synchronous";
 		  boolean scoped = scope != null;
-		
+
+		  String start = tundra.datetime.now();
 		  Object value = invoke(service, scoped ? scope : pipeline, mode);
+		  String end = tundra.datetime.now();
+
 		  String key = mode.equals("asynchronous")? "$thread" : "$pipeline";
-		
+
 		  if (scoped || mode.equals("asynchronous")) {
 		    IDataUtil.put(cursor, key, value);
 		  }
+		  if (mode.equals("synchronous")) IDataUtil.put(cursor, "$duration", tundra.datetime.duration(start, end));
 		} finally {
 		  cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -188,17 +193,17 @@ public final class service
 		// [i] object:0:optional $thread
 		// [o] record:0:optional $pipeline
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  com.wm.app.b2b.server.ServiceThread thread = (com.wm.app.b2b.server.ServiceThread)IDataUtil.get(cursor, "$thread");
-		
+
 		  if (thread != null) IDataUtil.put(cursor, "$pipeline", join(thread));
 		} finally {
 		  cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -211,7 +216,7 @@ public final class service
 		// @sigtype java 3.5
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -229,7 +234,7 @@ public final class service
 		// [i] field:0:optional $content.type
 		// [i] field:0:optional $encoding
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  int code = Integer.parseInt(IDataUtil.getString(cursor, "$code"));
 		  String message = IDataUtil.getString(cursor, "$message");
@@ -237,14 +242,14 @@ public final class service
 		  Object content = IDataUtil.get(cursor, "$content");
 		  String contentType = IDataUtil.getString(cursor, "$content.type");
 		  String encoding = IDataUtil.getString(cursor, "$encoding");
-		
+
 		  respond(code, message, headers, content, contentType, encoding);
 		} finally {
 		  cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -257,7 +262,7 @@ public final class service
 		// @sigtype java 3.5
 		// [o] field:0:optional $self
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  IDataUtil.put(cursor, "$self", self());
 		} finally {
@@ -265,7 +270,7 @@ public final class service
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -278,7 +283,7 @@ public final class service
 		// @sigtype java 3.5
 		// [i] field:0:required $duration
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String duration = IDataUtil.getString(cursor, "$duration");
 		  sleep(duration);
@@ -287,7 +292,7 @@ public final class service
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -302,7 +307,7 @@ public final class service
 		// [i] field:0:optional $raise? {&quot;false&quot;,&quot;true&quot;}
 		// [o] field:0:required $valid?
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String service = IDataUtil.getString(cursor, "$service");
 		  boolean raise = tundra.bool.parse(IDataUtil.getString(cursor, "$raise?"));
@@ -312,7 +317,7 @@ public final class service
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 	// --- <<IS-START-SHARED>> ---
@@ -320,30 +325,30 @@ public final class service
 	public static void respond(int code, String message, IData headers, Object content, String contentType, String encoding) throws ServiceException {
 	  try {
 	    com.wm.net.HttpHeader response = com.wm.app.b2b.server.Service.getHttpResponseHeader();
-	
-	    if (response == null) { 
+
+	    if (response == null) {
 	      // not invoked via HTTP, so throw an exception instead for HTTP statuses >= 400
 	      if (code >= 400) tundra.exception.raise(tundra.string.normalize(content, encoding));
 	    } else {
 	      if (message == null) message = tundra.support.http.status(code);
 	      response.setResponse(code, message);
-	
+
 	      if (contentType == null) contentType = "application/octet-stream";
 	      if (encoding == null) encoding = tundra.support.constant.DEFAULT_CHARACTER_ENCODING;
-	
+
 	      javax.activation.MimeType mimeType = new javax.activation.MimeType(contentType);
 	      mimeType.setParameter("charset", encoding);
-	
+
 	      response.clearField("Content-Type");
 	      response.addField("Content-Type", mimeType.toString());
-	
+
 	      if (headers != null) {
 	        IDataCursor hc = headers.getCursor();
-	        
+
 	        while(hc.next()) {
 	          String key = hc.getKey();
 	          Object value = hc.getValue();
-	
+
 	          if (key != null && value != null) {
 	            response.clearField(key);
 	            response.addField(key, value.toString());
@@ -352,7 +357,7 @@ public final class service
 	        hc.destroy();
 	      }
 	    }
-	
+
 	    if (content == null) content = "";
 	    byte[] body = tundra.bytes.normalize(content, encoding);
 	    com.wm.app.b2b.server.Service.setResponse(body);
@@ -362,27 +367,27 @@ public final class service
 	    tundra.exception.raise(ex);
 	  }
 	}
-	
-	// returns true if the calling service is the top-level initiating 
+
+	// returns true if the calling service is the top-level initiating
 	// service of the current thread
 	public static boolean initiator() {
 	  return callstack().length <= 1;
 	}
-	
+
 	// returns true if the given string is a service and exists
 	public static boolean validate(String service) throws ServiceException {
 	  return validate(service, false);
 	}
-	
+
 	// returns true if the given string is a service and exists
 	public static boolean validate(String service, boolean raise) throws ServiceException {
 	  boolean valid = tundra.node.exists(service) && "service".equals(tundra.node.type(service));
-	
+
 	  if (raise && !valid) throw new ServiceException("Service does not exist: " + service);
-	  
+
 	  return valid;
 	}
-	
+
 	// returns the invocation call stack
 	public static String[] callstack() {
 	  java.util.Iterator stack = com.wm.app.b2b.server.InvokeState.getCurrentState().getCallStack().iterator();
@@ -394,7 +399,7 @@ public final class service
 	  if (services.size() > 0) services.remove(services.size() - 1);
 	  return (String[])services.toArray(new String[services.size()]);
 	}
-	
+
 	// returns the name of the current service
 	public static String self() {
 	  String self = null;
@@ -402,14 +407,14 @@ public final class service
 	  if (callstack.length > 0) self = tundra.list.object.get(callstack, -1); // last element in list
 	  return self;
 	}
-	
+
 	// invokes the given service synchronously
-	public static IData invoke(String service, IData pipeline) throws ServiceException {  
+	public static IData invoke(String service, IData pipeline) throws ServiceException {
 	  return (IData)invoke(service, pipeline, "synchronous");
 	}
-	
+
 	// invokes the given service either synchronously or asynchronously
-	public static Object invoke(String service, IData pipeline, String mode) throws ServiceException {  
+	public static Object invoke(String service, IData pipeline, String mode) throws ServiceException {
 	  Object result = null;
 	  if (mode.equals("synchronous")) {
 	    result = invoke.synchronous(service, pipeline);
@@ -420,44 +425,44 @@ public final class service
 	  }
 	  return result;
 	}
-	
+
 	public static class invoke {
 	  // invokes a service asynchronously
 	  public static com.wm.app.b2b.server.ServiceThread asynchronous(String service, IData pipeline) {
 	    if (pipeline == null) pipeline = IDataFactory.create();
 	    if (service == null) return null;
-	    
+
 	    IData scope = IDataUtil.clone(pipeline);
-	    com.wm.lang.ns.NSName name = com.wm.lang.ns.NSName.create(service);  
+	    com.wm.lang.ns.NSName name = com.wm.lang.ns.NSName.create(service);
 	    com.wm.app.b2b.server.ServiceThread thread = com.wm.app.b2b.server.Service.doThreadInvoke(name, scope);
-	
+
 	    return thread;
 	  }
-	
+
 	  // invokes a service synchronously
 	  public static IData synchronous(String service, IData pipeline) throws ServiceException {
 	    if (pipeline == null) pipeline = IDataFactory.create();
 	    if (service == null) return pipeline;
-	    
+
 	    IData scope = IDataUtil.clone(pipeline);
 	    com.wm.lang.ns.NSName name = com.wm.lang.ns.NSName.create(service);
-	
+
 	    try {
 	      scope = com.wm.app.b2b.server.Service.doInvoke(name, scope);
 	    } catch (Exception ex) {
 	      tundra.exception.raise(ex);
 	    }
-	
+
 	    IDataUtil.merge(scope, pipeline);
-	    
+
 	    return pipeline;
 	  }
 	}
-	
+
 	// waits for an asynchronously invoked service to complete
 	public static IData join(com.wm.app.b2b.server.ServiceThread thread) throws ServiceException {
 	  IData pipeline = IDataFactory.create();
-	
+
 	  if (thread != null) {
 	    try {
 	      pipeline = thread.getIData();
@@ -465,15 +470,15 @@ public final class service
 	      tundra.exception.raise(ex);
 	    }
 	  }
-	  
+
 	  return pipeline;
 	}
-	
+
 	// sleeps the current thread for the given duration
 	public static void sleep(String duration) throws ServiceException {
 	  sleep(tundra.duration.parse(duration).getTimeInMillis(new java.util.Date()));
 	}
-	
+
 	// sleeps the current thread for the given duration
 	public static void sleep(long milliseconds) throws ServiceException {
 	  try {
@@ -482,7 +487,7 @@ public final class service
 	    tundra.exception.raise(ex);
 	  }
 	}
-	
+
 	// provides a try/catch/finally pattern for flow services
 	public static IData ensure(String service, IData pipeline, String catchService, String finallyService) throws ServiceException {
 	  try {
@@ -495,7 +500,7 @@ public final class service
 	    IDataUtil.put(cursor, "$exception.message", t.getMessage());
 	    IDataUtil.put(cursor, "$exception.stack", tundra.exception.stack(t));
 	    cursor.destroy();
-	
+
 	    if (catchService == null) {
 	      tundra.exception.raise(t);
 	    } else {
@@ -504,45 +509,45 @@ public final class service
 	  } finally {
 	    if (finallyService != null) pipeline = invoke(finallyService, pipeline);
 	  }
-	
+
 	  return pipeline;
 	}
-	
+
 	// invokes the given service a given number of times, and returns execution duration statistics
 	public static IncrementalNormalDistributionEstimator benchmark(String service, IData pipeline, int count) throws ServiceException {
 	  IncrementalNormalDistributionEstimator estimator = new IncrementalNormalDistributionEstimator("ms");
-	
+
 	  validate(service, true);
-	
-	  try { 
+
+	  try {
 	    for (int i = 0; i < count; i++) {
 	      long start = System.currentTimeMillis();
 	      tundra.service.invoke.synchronous(service, pipeline);
 	      long end = System.currentTimeMillis();
-	
+
 	      estimator.append(end - start);
 	    }
 	  } catch (ServiceException ex) {
 	    // ignore exceptions
 	  }
-	
+
 	  return estimator;
 	}
-	
+
 	// Class for incrementally calculating the mean and standard deviation
 	public static class IncrementalNormalDistributionEstimator {
-	
+
 	  protected long count;
 	  protected double mean, sq;
 	  protected String unit = "";
-	
+
 	  /**
 	   * Constructs a new estimator object.
 	   */
 	  public IncrementalNormalDistributionEstimator() {
 	    reset();
 	  }
-	
+
 	  /**
 	   * Constructs a new estimator object.
 	   * @param unit The unit of measurement related to the measured samples.
@@ -550,8 +555,8 @@ public final class service
 	  public IncrementalNormalDistributionEstimator(String unit) {
 	    this();
 	    this.unit = unit;
-	  }  
-	
+	  }
+
 	  /**
 	   * Constructs a new estimator object seeded with the given samples.
 	   *
@@ -561,7 +566,7 @@ public final class service
 	    this();
 	    append(samples);
 	  }
-	
+
 	  /**
 	   * Constructs a new estimator object seeded with the given samples.
 	   *
@@ -572,7 +577,7 @@ public final class service
 	    this(unit);
 	    append(samples);
 	  }
-	
+
 	  /**
 	   * Constructs a new estimator object seeded with the given collection of
 	   * samples.
@@ -583,7 +588,7 @@ public final class service
 	    this();
 	    append(samples);
 	  }
-	
+
 	  /**
 	   * Constructs a new estimator object seeded with the given collection of
 	   * samples.
@@ -595,7 +600,7 @@ public final class service
 	    this(unit);
 	    append(samples);
 	  }
-	
+
 	  /**
 	   * Appends the given sample to the list of samples in the estimator.
 	   *
@@ -606,10 +611,10 @@ public final class service
 	    double next = mean + (sample - mean) / ++count;
 	    sq += (sample - mean) * (sample - next);
 	    mean = next;
-	
+
 	    return this;
 	  }
-	
+
 	  /**
 	   * Adds one or more samples to the estimator.
 	   *
@@ -620,10 +625,10 @@ public final class service
 	    for (double sample : samples) {
 	      append(sample);
 	    }
-	
+
 	    return this;
 	  }
-	
+
 	  /**
 	   * Adds a collection of samples to the estimator.
 	   *
@@ -634,10 +639,10 @@ public final class service
 	    for (double sample : samples) {
 	      append(sample);
 	    }
-	
+
 	    return this;
 	  }
-	
+
 	  /**
 	   * Returns the number of samples seen by this estimator.
 	   *
@@ -646,7 +651,7 @@ public final class service
 	  public long count() {
 	    return count;
 	  }
-	
+
 	  /**
 	   * Returns the mean of the samples.
 	   *
@@ -655,7 +660,7 @@ public final class service
 	  public double mean() {
 	    return mean;
 	  }
-	
+
 	  /**
 	   * Returns the maximum likelihood estimate of the variance of the samples.
 	   *
@@ -664,7 +669,7 @@ public final class service
 	  public double variance() {
 	    return count > 1 && mean > 0 ? sq / mean : 0.0;
 	  }
-	
+
 	  /**
 	   * Returns the maximum likelihood estimate of the standard deviation of the
 	   * samples.
@@ -674,7 +679,7 @@ public final class service
 	  public double standardDeviation() {
 	    return Math.sqrt(variance());
 	  }
-	
+
 	  /**
 	   * Returns the unit of measure related to the measured samples.
 	   *
@@ -683,7 +688,7 @@ public final class service
 	  public String unit() {
 	    return unit;
 	  }
-	
+
 	  /**
 	   * Resets the estimator back to zero samples.
 	   *
@@ -693,10 +698,10 @@ public final class service
 	    count = 0;
 	    mean = 0.0;
 	    sq = 0.0;
-	
+
 	    return this;
 	  }
-	
+
 	  /**
 	   * Returns a string-based representation of the mean, standard deviation and
 	   * number of samples for this estimator.
