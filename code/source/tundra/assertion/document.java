bc71a142d238e5e69a117f20a388a28a77bae289
Lachlan Dowding <lachlan@permafro.st>
2015-07-08 20:40:24 +1000

    Reindent tundra.assertion.document java source

--- code/source/tundra/assertion/document.java
+++ code/source/tundra/assertion/document.java
@@ -1,8 +1,8 @@
 package tundra.assertion;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2015-05-03 16:07:13 EST
-// -----( ON-HOST: -
+// -----( CREATED: 2015-07-08 20:39:28 AEST
+// -----( ON-HOST: 192.168.66.129
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -38,19 +38,19 @@ public final class document
 		// [i] record:0:required $actual
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  IData expected = IDataUtil.getIData(cursor, "$expected");
-		  IData actual = IDataUtil.getIData(cursor, "$actual");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  equal(expected, actual, message);
+		    IData expected = IDataUtil.getIData(cursor, "$expected");
+		    IData actual = IDataUtil.getIData(cursor, "$actual");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    equal(expected, actual, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -65,44 +65,44 @@ public final class document
 		// [i] record:0:required $actual
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  IData expected = IDataUtil.getIData(cursor, "$expected");
-		  IData actual = IDataUtil.getIData(cursor, "$actual");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  unequal(expected, actual, message);
+		    IData expected = IDataUtil.getIData(cursor, "$expected");
+		    IData actual = IDataUtil.getIData(cursor, "$actual");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    unequal(expected, actual, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 	// --- <<IS-START-SHARED>> ---
 	// asserts that two documents are equal
 	public static void equal(IData expected, IData actual, String message) {
-	  if (!ObjectHelper.equal(expected, actual)) {
-	    if (message == null) {
-	      message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is not equal to actual '{'{1}'}'", expected, actual);
-	    } else {
-	      message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is not equal to actual '{'{2}'}')", message, expected, actual);
+	    if (!ObjectHelper.equal(expected, actual)) {
+	        if (message == null) {
+	            message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is not equal to actual '{'{1}'}'", expected, actual);
+	        } else {
+	            message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is not equal to actual '{'{2}'}')", message, expected, actual);
+	        }
+	        throw new AssertionError(message);
 	    }
-	    throw new AssertionError(message);
-	  }
 	}
-
-	// asserts that two documents are not equal
+	
+	// asserts that two documents are not equal 
 	public static void unequal(IData expected, IData actual, String message) {
-	  if (ObjectHelper.equal(expected, actual)) {
-	    if (message == null) {
-	      message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is equal to actual '{'{1}'}'", expected, actual);
-	    } else {
-	      message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is equal to actual '{'{2}'}')", message, expected, actual);
+	    if (ObjectHelper.equal(expected, actual)) {
+	        if (message == null) {
+	            message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is equal to actual '{'{1}'}'", expected, actual);
+	        } else {
+	            message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is equal to actual '{'{2}'}')", message, expected, actual);
+	        }
+	        throw new AssertionError(message);
 	    }
-	    throw new AssertionError(message);
-	  }
 	}
 	// --- <<IS-END-SHARED>> ---
 }
