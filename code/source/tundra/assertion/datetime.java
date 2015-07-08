07029570438f329d7f38ac2c776bee478b4a8677
Lachlan Dowding <lachlan@permafro.st>
2015-07-08 20:38:11 +1000

    Reindent tundra.assertion.datetime java source

--- code/source/tundra/assertion/datetime.java
+++ code/source/tundra/assertion/datetime.java
@@ -1,8 +1,8 @@
 package tundra.assertion;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2013-09-06 09:14:34.490
-// -----( ON-HOST: -
+// -----( CREATED: 2015-07-08 14:12:25 AEST
+// -----( ON-HOST: 192.168.66.129
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -35,19 +35,19 @@ public final class datetime
 		// @sigtype java 3.5
 		// [i] field:0:required $expected
 		// [i] field:0:required $actual
-		// [i] field:0:optional $pattern {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $pattern {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
 		
 		try {
-		  String expected = IDataUtil.getString(cursor, "$expected");
-		  String actual = IDataUtil.getString(cursor, "$actual");
-		  String pattern = IDataUtil.getString(cursor, "$pattern");
-		  String message = IDataUtil.getString(cursor, "$message");
+		    String expected = IDataUtil.getString(cursor, "$expected");
+		    String actual = IDataUtil.getString(cursor, "$actual");
+		    String pattern = IDataUtil.getString(cursor, "$pattern");
+		    String message = IDataUtil.getString(cursor, "$message");
 		
-		  equal(expected, actual, pattern, message);
+		    equal(expected, actual, pattern, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
@@ -64,19 +64,19 @@ public final class datetime
 		// @sigtype java 3.5
 		// [i] field:0:required $expected
 		// [i] field:0:required $actual
-		// [i] field:0:optional $pattern {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $pattern {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
 		
 		try {
-		  String expected = IDataUtil.getString(cursor, "$expected");
-		  String actual = IDataUtil.getString(cursor, "$actual");
-		  String pattern = IDataUtil.getString(cursor, "$pattern");
-		  String message = IDataUtil.getString(cursor, "$message");
+		    String expected = IDataUtil.getString(cursor, "$expected");
+		    String actual = IDataUtil.getString(cursor, "$actual");
+		    String pattern = IDataUtil.getString(cursor, "$pattern");
+		    String message = IDataUtil.getString(cursor, "$message");
 		
-		  unequal(expected, actual, pattern, message);
+		    equal(expected, actual, pattern, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
@@ -86,12 +86,12 @@ public final class datetime
 	// --- <<IS-START-SHARED>> ---
 	// asserts that two datetime strings formatted according to the given pattern are equal
 	public static void equal(String expected, String actual, String pattern, String message) {
-	  tundra.assertion.object.equal(tundra.datetime.parse(expected, pattern), tundra.datetime.parse(actual, pattern), message);
+	    tundra.assertion.object.equal(tundra.datetime.parse(expected, pattern), tundra.datetime.parse(actual, pattern), message);
 	}
 	
 	// asserts that two datetime strings formatted according to the given pattern are not equal
 	public static void unequal(String expected, String actual, String pattern, String message) {
-	  tundra.assertion.object.unequal(tundra.datetime.parse(expected, pattern), tundra.datetime.parse(actual, pattern), message);
+	    tundra.assertion.object.unequal(tundra.datetime.parse(expected, pattern), tundra.datetime.parse(actual, pattern), message);
 	}
 	// --- <<IS-END-SHARED>> ---
 }
