fb8c3446f893f766549349f5841325989670577a
Lachlan Dowding <lachlan@permafro.st>
2015-07-08 21:03:38 +1000

    Reindent tundra.assertion.object java source

--- code/source/tundra/assertion/object.java
+++ code/source/tundra/assertion/object.java
@@ -1,8 +1,8 @@
 package tundra.assertion;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2012-05-12 17:07:42 EST
-// -----( ON-HOST: -
+// -----( CREATED: 2015-07-08 21:02:49 AEST
+// -----( ON-HOST: 192.168.66.129
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -33,23 +33,23 @@ public final class object
 		// --- <<IS-START(equal)>> ---
 		// @subtype unknown
 		// @sigtype java 3.5
-		// [i] object:0:optional $expected
-		// [i] object:0:optional $actual
+		// [i] object:0:required $expected
+		// [i] object:0:required $actual
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  Object expected = IDataUtil.get(cursor, "$expected");
-		  Object actual = IDataUtil.get(cursor, "$actual");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  equal(expected, actual, message);
+		    Object expected = IDataUtil.get(cursor, "$expected");
+		    Object actual = IDataUtil.get(cursor, "$actual");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    equal(expected, actual, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -63,18 +63,18 @@ public final class object
 		// [i] object:0:optional $object
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  Object object = IDataUtil.get(cursor, "$object");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  exist(object, message);
+		    Object object = IDataUtil.get(cursor, "$object");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    exist(object, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -85,23 +85,23 @@ public final class object
 		// --- <<IS-START(instance)>> ---
 		// @subtype unknown
 		// @sigtype java 3.5
-		// [i] object:0:optional $object
+		// [i] object:0:required $object
 		// [i] field:0:required $class
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  Object object = IDataUtil.get(cursor, "$object");
-		  String className = IDataUtil.getString(cursor, "$class");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  instance(object, className, message);
+		    Object object = IDataUtil.get(cursor, "$object");
+		    String className = IDataUtil.getString(cursor, "$class");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    instance(object, className, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -115,18 +115,18 @@ public final class object
 		// [i] object:0:optional $object
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  Object object = IDataUtil.get(cursor, "$object");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  nothing(object, message);
+		    Object object = IDataUtil.get(cursor, "$object");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    nothing(object, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -137,90 +137,90 @@ public final class object
 		// --- <<IS-START(unequal)>> ---
 		// @subtype unknown
 		// @sigtype java 3.5
-		// [i] object:0:optional $expected
-		// [i] object:0:optional $actual
+		// [i] object:0:required $expected
+		// [i] object:0:required $actual
 		// [i] field:0:optional $message
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  Object expected = IDataUtil.get(cursor, "$expected");
-		  Object actual = IDataUtil.get(cursor, "$actual");
-		  String message = IDataUtil.getString(cursor, "$message");
-
-		  unequal(expected, actual, message);
+		    Object expected = IDataUtil.get(cursor, "$expected");
+		    Object actual = IDataUtil.get(cursor, "$actual");
+		    String message = IDataUtil.getString(cursor, "$message");
+		
+		    unequal(expected, actual, message);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 	// --- <<IS-START-SHARED>> ---
 	// asserts that two objects are equal
 	public static void equal(Object expected, Object actual, String message) {
-	  if (!((expected == null && actual == null) || (expected != null && actual != null && expected.equals(actual)))) {
-	    if (message == null) {
-	      message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is not equal to actual '{'{1}'}'", expected, actual);
-	    } else {
-	      message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is not equal to actual '{'{2}'}')", message, expected, actual);
+	    if (!((expected == null && actual == null) || (expected != null && actual != null && expected.equals(actual)))) {
+	        if (message == null) {
+	            message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is not equal to actual '{'{1}'}'", expected, actual);
+	        } else {
+	            message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is not equal to actual '{'{2}'}')", message, expected, actual);
+	        }
+	        throw new AssertionError(message);
 	    }
-	    throw new AssertionError(message);
-	  }
 	}
-
-	// asserts that two objects are not equal
+	
+	// asserts that two objects are not equal 
 	public static void unequal(Object expected, Object actual, String message) {
-	  if ((expected == null && actual == null) || (expected != null && actual != null && expected.equals(actual))) {
-	    if (message == null) {
-	      message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is equal to actual '{'{1}'}'", expected, actual);
-	    } else {
-	      message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is equal to actual '{'{2}'}')", message, expected, actual);
+	    if ((expected == null && actual == null) || (expected != null && actual != null && expected.equals(actual))) {
+	        if (message == null) {
+	            message = java.text.MessageFormat.format("Assertion failed: expected '{'{0}'}' is equal to actual '{'{1}'}'", expected, actual);
+	        } else {
+	            message = java.text.MessageFormat.format("Assertion failed: {0} (expected '{'{1}'}' is equal to actual '{'{2}'}')", message, expected, actual);
+	        }
+	        throw new AssertionError(message);
 	    }
-	    throw new AssertionError(message);
-	  }
 	}
-
+	
 	// asserts that the given object is an instance of the given class
 	public static void instance(Object object, String className, String message) {
-	  if (message == null) {
-	    message = java.text.MessageFormat.format("Assertion failed: object '{'{0}'}' is not an instance of class '{'{1}'}'", object, className);
-	  } else {
-	    message = java.text.MessageFormat.format("Assertion failed: {0} (object '{'{1}'}' is not an instance of class '{'{2}'}')", message, object, className);
-	  }
-
-	  try {
-	    Class klass = Class.forName(className);
-	    if (!klass.isInstance(object)) {
-	      throw new AssertionError(message);
+	    if (message == null) {
+	        message = java.text.MessageFormat.format("Assertion failed: object '{'{0}'}' is not an instance of class '{'{1}'}'", object, className);
+	    } else {
+	        message = java.text.MessageFormat.format("Assertion failed: {0} (object '{'{1}'}' is not an instance of class '{'{2}'}')", message, object, className);
 	    }
-	  } catch (ClassNotFoundException ex) {
-	    throw new AssertionError(message);
-	  }
+	
+	    try {
+	        Class klass = Class.forName(className);
+	        if (!klass.isInstance(object)) {
+	            throw new AssertionError(message);
+	        }
+	    } catch (ClassNotFoundException ex) {
+	        throw new AssertionError(message);
+	    }
 	}
-
+	
 	// asserts that the given object is null
 	public static void nothing(Object object, String message) {
-	  if (object != null) {
-	    if (message == null) {
-	      message = java.text.MessageFormat.format("Assertion failed: object is not null '{'{0}'}'", object);
-	    } else {
-	      message = java.text.MessageFormat.format("Assertion failed: {0} (object is not null '{'{1}'}')", message, object);
+	    if (object != null) {
+	        if (message == null) {
+	            message = java.text.MessageFormat.format("Assertion failed: object is not null '{'{0}'}'", object);
+	        } else {
+	            message = java.text.MessageFormat.format("Assertion failed: {0} (object is not null '{'{1}'}')", message, object);
+	        }
+	        throw new AssertionError(message);
 	    }
-	    throw new AssertionError(message);
-	  }
 	}
-
+	
 	// asserts that the given object is not null
 	public static void exist(Object object, String message) {
-	  if (object == null) {
-	    if (message == null) {
-	      message = java.text.MessageFormat.format("Assertion failed: object is null '{'{0}'}'", object);
-	    } else {
-	      message = java.text.MessageFormat.format("Assertion failed: {0} (object is null '{'{1}'}')", message, object);
+	    if (object == null) {
+	        if (message == null) {
+	            message = java.text.MessageFormat.format("Assertion failed: object is null '{'{0}'}'", object);
+	        } else {
+	            message = java.text.MessageFormat.format("Assertion failed: {0} (object is null '{'{1}'}')", message, object);
+	        }
+	        throw new AssertionError(message);
 	    }
-	    throw new AssertionError(message);
-	  }
 	}
 	// --- <<IS-END-SHARED>> ---
 }
--- ns/tundra/assertion/object/exists/java.frag
+++ ns/tundra/assertion/object/exists/java.frag
@@ -3,8 +3,9 @@
 <Values version="2.0">
   <value name="name">exists</value>
   <value name="encodeutf8">true</value>
-  <value name="body">SURhdGFDdXJzb3IgY3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7Cgp0cnkgewogIE9iamVj
-dCBvYmplY3QgPSBJRGF0YVV0aWwuZ2V0KGN1cnNvciwgIiRvYmplY3QiKTsKICBTdHJpbmcgbWVz
-c2FnZSA9IElEYXRhVXRpbC5nZXRTdHJpbmcoY3Vyc29yLCAiJG1lc3NhZ2UiKTsKICAKICBleGlz
-dChvYmplY3QsIG1lc3NhZ2UpOwp9IGZpbmFsbHkgewogIGN1cnNvci5kZXN0cm95KCk7Cn0=</value>
+  <value name="body">SURhdGFDdXJzb3IgY3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7DQoNCnRyeSB7DQogICAg
+T2JqZWN0IG9iamVjdCA9IElEYXRhVXRpbC5nZXQoY3Vyc29yLCAiJG9iamVjdCIpOw0KICAgIFN0
+cmluZyBtZXNzYWdlID0gSURhdGFVdGlsLmdldFN0cmluZyhjdXJzb3IsICIkbWVzc2FnZSIpOw0K
+DQogICAgZXhpc3Qob2JqZWN0LCBtZXNzYWdlKTsNCn0gZmluYWxseSB7DQogICAgY3Vyc29yLmRl
+c3Ryb3koKTsNCn0=</value>
 </Values>
