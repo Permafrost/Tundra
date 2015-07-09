9032728c93993dda67daa400be86c0fb9a2efcb3
Lachlan Dowding <lachlan@permafro.st>
2015-07-09 14:53:09 +1000

    Reindent tundra.list.datetime java source

--- code/source/tundra/list/datetime.java
+++ code/source/tundra/list/datetime.java
@@ -1,8 +1,8 @@
 package tundra.list;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2014-11-21 22:27:14 EST
-// -----( ON-HOST: -
+// -----( CREATED: 2015-07-09 14:52:48 AEST
+// -----( ON-HOST: 192.168.66.129
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -34,60 +34,60 @@ public final class datetime
 		// @subtype unknown
 		// @sigtype java 3.5
 		// [i] field:1:optional $list
-		// [i] field:0:optional $pattern.input {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $pattern.input {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [i] field:1:optional $patterns.input
-		// [i] field:0:optional $pattern.output {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $pattern.output {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [i] field:0:optional $timezone.input
 		// [i] field:0:optional $timezone.output
 		// [o] field:1:optional $list
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  String[] list = IDataUtil.getStringArray(cursor, "$list");
-		  String inPattern = IDataUtil.getString(cursor, "$pattern.input");
-		  String[] inPatterns = IDataUtil.getStringArray(cursor, "$patterns.input");
-		  String outPattern = IDataUtil.getString(cursor, "$pattern.output");
-		  String inTimeZone = IDataUtil.getString(cursor, "$timezone.input");
-		  String outTimeZone = IDataUtil.getString(cursor, "$timezone.output");
-
-		  if (list != null && list.length > 0) {
-		    if (inPatterns == null) {
-		      IDataUtil.put(cursor, "$list", format(list, inPattern, inTimeZone, outPattern, outTimeZone));
-		    } else {
-		      IDataUtil.put(cursor, "$list", format(list, inPatterns, inTimeZone, outPattern, outTimeZone));
+		    String[] list = IDataUtil.getStringArray(cursor, "$list");
+		    String inPattern = IDataUtil.getString(cursor, "$pattern.input");
+		    String[] inPatterns = IDataUtil.getStringArray(cursor, "$patterns.input");
+		    String outPattern = IDataUtil.getString(cursor, "$pattern.output");
+		    String inTimeZone = IDataUtil.getString(cursor, "$timezone.input");
+		    String outTimeZone = IDataUtil.getString(cursor, "$timezone.output");
+		
+		    if (list != null && list.length > 0) {
+		        if (inPatterns == null) {
+		            IDataUtil.put(cursor, "$list", format(list, inPattern, inTimeZone, outPattern, outTimeZone));
+		        } else {
+		            IDataUtil.put(cursor, "$list", format(list, inPatterns, inTimeZone, outPattern, outTimeZone));
+		        }
 		    }
-		  }
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 	// --- <<IS-START-SHARED>> ---
 	// formats a list datetime strings to the given pattern
 	public static String[] format(String[] inputs, String inPattern, String inTimeZone, String outPattern, String outTimeZone) {
-	  String[] outputs = null;
-	  if (inputs != null) {
-	    outputs = new String[inputs.length];
-	    for (int i = 0; i < inputs.length; i++) {
-	      outputs[i] = tundra.datetime.format(inputs[i], inPattern, inTimeZone, outPattern, outTimeZone);
+	    String[] outputs = null;
+	    if (inputs != null) {
+	        outputs = new String[inputs.length];
+	        for (int i = 0; i < inputs.length; i++) {
+	            outputs[i] = tundra.datetime.format(inputs[i], inPattern, inTimeZone, outPattern, outTimeZone);
+	        }
 	    }
-	  }
-	  return outputs;
+	    return outputs;
 	}
-
+	
 	// formats a list datetime strings to the given pattern
 	public static String[] format(String[] inputs, String[] inPatterns, String inTimeZone, String outPattern, String outTimeZone) {
-	  String[] outputs = null;
-	  if (inputs != null) {
-	    outputs = new String[inputs.length];
-	    for (int i = 0; i < inputs.length; i++) {
-	      outputs[i] = tundra.datetime.format(inputs[i], inPatterns, inTimeZone, outPattern, outTimeZone);
+	    String[] outputs = null;
+	    if (inputs != null) {
+	        outputs = new String[inputs.length];
+	        for (int i = 0; i < inputs.length; i++) {
+	            outputs[i] = tundra.datetime.format(inputs[i], inPatterns, inTimeZone, outPattern, outTimeZone);
+	        }
 	    }
-	  }
-	  return outputs;
+	    return outputs;
 	}
 	// --- <<IS-END-SHARED>> ---
 }
