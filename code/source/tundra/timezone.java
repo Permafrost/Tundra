3266507b6e98170b7f7016ba8762163d9397cfd8
Lachlan Dowding <lachlan@permafro.st>
2015-07-09 10:28:42 +1000

    Reimplement tundra.timezone:* services using Tundra.java TimeZoneHelper methods

--- code/source/tundra/timezone.java
+++ code/source/tundra/timezone.java
@@ -1,14 +1,15 @@
 package tundra;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2014-09-28 20:14:43 EST
-// -----( ON-HOST: -
+// -----( CREATED: 2015-07-09 10:27:38 AEST
+// -----( ON-HOST: 192.168.66.129
 
 import com.wm.data.*;
 import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
+import permafrost.tundra.time.TimeZoneHelper;
 // --- <<IS-END-IMPORTS>> ---
 
 public final class timezone
@@ -35,7 +36,7 @@ public final class timezone
 		// @sigtype java 3.5
 		// [i] field:0:required $id
 		// [i] field:0:optional $datetime
-		// [i] field:0:optional $datetime.pattern {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $datetime.pattern {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [o] record:0:optional $timezone
 		// [o] - field:0:required id
 		// [o] - field:0:required name
@@ -45,21 +46,21 @@ public final class timezone
 		// [o] - field:0:required dst.active?
 		// [o] - field:0:required dst.offset
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  String id = IDataUtil.getString(cursor, "$id");
-		  String datetime = IDataUtil.getString(cursor, "$datetime");
-		  String pattern = IDataUtil.getString(cursor, "$datetime.pattern");
-
-		  IData timezone = get(id, datetime, pattern);
-
-		  if (timezone != null) IDataUtil.put(cursor, "$timezone", timezone);
+		    String id = IDataUtil.getString(cursor, "$id");
+		    String datetime = IDataUtil.getString(cursor, "$datetime");
+		    String pattern = IDataUtil.getString(cursor, "$datetime.pattern");
+		
+		    IData timezone = TimeZoneHelper.toIData(TimeZoneHelper.get(id), datetime, pattern);
+		
+		    if (timezone != null) IDataUtil.put(cursor, "$timezone", timezone);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -71,7 +72,7 @@ public final class timezone
 		// @subtype unknown
 		// @sigtype java 3.5
 		// [i] field:0:optional $datetime
-		// [i] field:0:optional $datetime.pattern {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $datetime.pattern {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [o] record:1:optional $timezones
 		// [o] - field:0:required id
 		// [o] - field:0:required name
@@ -81,20 +82,20 @@ public final class timezone
 		// [o] - field:0:required dst.active?
 		// [o] - field:0:required dst.offset
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  String datetime = IDataUtil.getString(cursor, "$datetime");
-		  String pattern = IDataUtil.getString(cursor, "$datetime.pattern");
-
-		  IData[] timezones = list(datetime, pattern);
-
-		  if (timezones != null) IDataUtil.put(cursor, "$timezones", timezones);
+		    String datetime = IDataUtil.getString(cursor, "$datetime");
+		    String pattern = IDataUtil.getString(cursor, "$datetime.pattern");
+		
+		    IData[] timezones = TimeZoneHelper.toIDataArray(TimeZoneHelper.list(), datetime, pattern);
+		
+		    if (timezones != null) IDataUtil.put(cursor, "$timezones", timezones);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 
 
@@ -106,7 +107,7 @@ public final class timezone
 		// @subtype unknown
 		// @sigtype java 3.5
 		// [i] field:0:optional $datetime
-		// [i] field:0:optional $datetime.pattern {&quot;datetime&quot;,&quot;datetime.jdbc&quot;,&quot;date&quot;,&quot;date.jdbc&quot;,&quot;time&quot;,&quot;time.jdbc&quot;,&quot;milliseconds&quot;}
+		// [i] field:0:optional $datetime.pattern {"datetime","datetime.jdbc","date","date.jdbc","time","time.jdbc","milliseconds"}
 		// [o] record:0:optional $timezone
 		// [o] - field:0:required id
 		// [o] - field:0:required name
@@ -116,119 +117,20 @@ public final class timezone
 		// [o] - field:0:required dst.active?
 		// [o] - field:0:required dst.offset
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
-		  String datetime = IDataUtil.getString(cursor, "$datetime");
-		  String pattern = IDataUtil.getString(cursor, "$datetime.pattern");
-
-		  IData timezone = self(datetime, pattern);
-
-		  if (timezone != null) IDataUtil.put(cursor, "$timezone", timezone);
+		    String datetime = IDataUtil.getString(cursor, "$datetime");
+		    String pattern = IDataUtil.getString(cursor, "$datetime.pattern");
+		
+		    IData timezone = TimeZoneHelper.toIData(TimeZoneHelper.self(), datetime, pattern);
+		
+		    if (timezone != null) IDataUtil.put(cursor, "$timezone", timezone);
 		} finally {
-		  cursor.destroy();
+		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
-
-	// --- <<IS-START-SHARED>> ---
-	// returns the time zone associated with the given ID in IData format
-	public static IData get(String id, String datetime) {
-	  return get(id, datetime, null);
-	}
-
-	// returns the time zone associated with the given ID in IData format
-	public static IData get(String id, String datetime, String pattern) {
-	  return get(id, instant(datetime, pattern));
-	}
-
-	// returns the time zone associated with the given ID in IData format
-	public static IData get(String id, java.util.Date instant) {
-	  return toIData(tundra.support.timezone.get(id), instant);
-	}
-
-	// returns the default time zone in IData format
-	public static IData self(String datetime) {
-	  return self(datetime, null);
-	}
-
-	// returns the default time zone in IData format
-	public static IData self(String datetime, String pattern) {
-	  return self(instant(datetime, pattern));
-	}
-
-	// returns the default time zone in IData format
-	public static IData self(java.util.Date instant) {
-	  return toIData(tundra.support.timezone.self(), instant);
-	}
-
-	// returns all known time zones in IData format
-	public static IData[] list(String datetime) {
-	  return list(datetime, null);
-	}
-
-	// returns all known time zones in IData format
-	public static IData[] list(String datetime, String pattern) {
-	  return list(instant(datetime, pattern));
-	}
-
-	// returns all known time zones in IData format
-	public static IData[] list(java.util.Date instant) {
-	  java.util.TimeZone[] zones = tundra.support.timezone.list();
-	  IData[] output = new IData[zones.length];
-
-	  for (int i = 0; i < zones.length; i++) {
-	    output[i] = toIData(zones[i], instant);
-	  }
-
-	  return output;
-	}
-
-	// returns the given timezone in IData format
-	protected static IData toIData(java.util.TimeZone timezone, java.util.Date instant) {
-	  if (timezone == null) return null;
-
-	  IData doc = IDataFactory.create();
-	  IDataCursor cursor = doc.getCursor();
-
-	  boolean dstActive = timezone.inDaylightTime(instant);
-
-	  IDataUtil.put(cursor, "id", timezone.getID());
-	  IDataUtil.put(cursor, "name", timezone.getDisplayName(dstActive, java.util.TimeZone.SHORT));
-	  IDataUtil.put(cursor, "description", timezone.getDisplayName(dstActive, java.util.TimeZone.LONG));
-	  IDataUtil.put(cursor, "utc.offset", tundra.duration.format("" + timezone.getOffset(instant.getTime()), "milliseconds", "xml"));
-	  IDataUtil.put(cursor, "dst.used?", "" + timezone.useDaylightTime());
-	  IDataUtil.put(cursor, "dst.active?", "" + dstActive);
-	  IDataUtil.put(cursor, "dst.offset", tundra.duration.format("" + timezone.getDSTSavings(), "milliseconds", "xml"));
-
-	  cursor.destroy();
-
-	  return doc;
-	}
-
-	// returns current date
-	protected static java.util.Date instant() {
-	  return instant(null);
-	}
-
-	// converts a datetime string to java.util.Date object, or returns current date if null
-	protected static java.util.Date instant(String datetime) {
-	  return instant(datetime, null);
-	}
-
-	// converts a datetime string to java.util.Date object, or returns current date if null
-	protected static java.util.Date instant(String datetime, String pattern) {
-	  java.util.Date instant = null;
-
-	  if (datetime == null) {
-	    instant = new java.util.Date();
-	  } else {
-	    instant = tundra.datetime.parse(datetime, pattern).getTime();
-	  }
-
-	  return instant;
-	}
-	// --- <<IS-END-SHARED>> ---
 }
 
