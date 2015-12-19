94ebe98b6bf981516fcaa543b9baa273303e708e
Lachlan Dowding <lachlan@permafro.st>
2015-12-19 16:29:01 +1000

    Change tundra.http.response:handle to throw a TransportException that includes the response body content

---
+++ code/source/tundra/support/exception/transport.java
@@ -0,0 +1,64 @@
+package tundra.support.exception;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2015-12-19 16:21:58 EST
+// -----( ON-HOST: 192.168.66.129
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import javax.activation.MimeTypeParseException;
+import java.io.IOException;
+import permafrost.tundra.lang.BytesHelper;
+import permafrost.tundra.lang.ExceptionHelper;
+import permafrost.tundra.lang.TransportException;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class transport
+
+{
+	// ---( internal utility methods )---
+
+	final static transport _instance = new transport();
+
+	static transport _newInstance() { return new transport(); }
+
+	static transport _cast(Object o) { return (transport)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void raise (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(raise)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] field:0:optional $message
+		// [i] object:0:optional $content
+		// [i] field:0:optional $content.type
+		IDataCursor cursor = pipeline.getCursor();
+		
+		try {
+		    String message = IDataUtil.getString(cursor, "$message");
+		    Object content = IDataUtil.get(cursor, "$content");
+		    String contentType = IDataUtil.getString(cursor, "$content.type");
+		
+		    throw new TransportException(message, BytesHelper.normalize(content), contentType);
+		} catch(IOException ex) {
+		    ExceptionHelper.raise(ex);
+		} catch(MimeTypeParseException ex) {
+		    ExceptionHelper.raise(ex);
+		} finally {
+		    cursor.destroy();
+		}
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ ns/tundra/support/exception/transport/raise/java.frag
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">raise</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">SURhdGFDdXJzb3IgY3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7Cgp0cnkgewogICAgU3Ry
+aW5nIG1lc3NhZ2UgPSBJRGF0YVV0aWwuZ2V0U3RyaW5nKGN1cnNvciwgIiRtZXNzYWdlIik7CiAg
+ICBPYmplY3QgY29udGVudCA9IElEYXRhVXRpbC5nZXQoY3Vyc29yLCAiJGNvbnRlbnQiKTsKICAg
+IFN0cmluZyBjb250ZW50VHlwZSA9IElEYXRhVXRpbC5nZXRTdHJpbmcoY3Vyc29yLCAiJGNvbnRl
+bnQudHlwZSIpOwoKICAgIHRocm93IG5ldyBUcmFuc3BvcnRFeGNlcHRpb24obWVzc2FnZSwgQnl0
+ZXNIZWxwZXIubm9ybWFsaXplKGNvbnRlbnQpLCBjb250ZW50VHlwZSk7Cn0gY2F0Y2goSU9FeGNl
+cHRpb24gZXgpIHsKICAgIEV4Y2VwdGlvbkhlbHBlci5yYWlzZShleCk7Cn0gY2F0Y2goTWltZVR5
+cGVQYXJzZUV4Y2VwdGlvbiBleCkgewogICAgRXhjZXB0aW9uSGVscGVyLnJhaXNlKGV4KTsKfSBm
+aW5hbGx5IHsKICAgIGN1cnNvci5kZXN0cm95KCk7Cn0=</value>
+</Values>
