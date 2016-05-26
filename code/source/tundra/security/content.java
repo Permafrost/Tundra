4f65dd6462df4ecb0a57b770ca0616a3d3b367f4
Lachlan Dowding <lachlan@permafro.st>
2016-05-27 07:54:58 +1000

    Change tundra.security.content:digest to use CharsetHelper.normalize and ObjectConvertMode.normalize

--- code/source/tundra/security/content.java
+++ code/source/tundra/security/content.java
@@ -1,8 +1,8 @@
 package tundra.security;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2016-03-22 10:43:14.488
-// -----( ON-HOST: -
+// -----( CREATED: 2016-05-27 07:53:49 EST
+// -----( ON-HOST: 192.168.66.129
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -10,10 +10,13 @@ import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import permafrost.tundra.lang.CharsetHelper;
 import permafrost.tundra.lang.ExceptionHelper;
+import permafrost.tundra.lang.ObjectConvertMode;
 import permafrost.tundra.lang.ObjectHelper;
 import permafrost.tundra.security.MessageDigestHelper;
 // --- <<IS-END-IMPORTS>> ---
@@ -42,20 +45,20 @@ public final class content
 		// @sigtype java 3.5
 		// [i] object:0:optional $content
 		// [i] field:0:optional $encoding
-		// [i] field:0:optional $algorithm {&quot;SHA-512&quot;,&quot;SHA-384&quot;,&quot;SHA-256&quot;,&quot;SHA&quot;,&quot;MD5&quot;,&quot;MD2&quot;}
-		// [i] field:0:optional $mode {&quot;stream&quot;,&quot;bytes&quot;,&quot;base64&quot;}
+		// [i] field:0:optional $algorithm {"SHA-512","SHA-384","SHA-256","SHA","MD5","MD2"}
+		// [i] field:0:optional $mode {"stream","bytes","base64"}
 		// [o] object:0:optional $content
 		// [o] object:0:optional $digest
 		IDataCursor cursor = pipeline.getCursor();
-
+		
 		try {
 		    Object content = IDataUtil.get(cursor, "$content");
-		    String charsetName = IDataUtil.getString(cursor, "$encoding");
-		    String algorithmName = IDataUtil.getString(cursor, "$algorithm");
-		    String mode = IDataUtil.getString(cursor, "$mode");
-
+		    Charset charset = CharsetHelper.normalize(IDataUtil.getString(cursor, "$encoding"));
+		    MessageDigest algorithm = MessageDigestHelper.normalize(IDataUtil.getString(cursor, "$algorithm"));
+		    ObjectConvertMode mode = ObjectConvertMode.normalize(IDataUtil.getString(cursor, "$mode"));
+		
 		    if (content != null) {
-		        Map.Entry<? extends Object, byte[]> digest = MessageDigestHelper.digest(MessageDigestHelper.normalize(algorithmName), content, CharsetHelper.normalize(charsetName));
+		        Map.Entry<? extends Object, byte[]> digest = MessageDigestHelper.digest(algorithm, content, charset);
 		        if (digest != null) {
 		            IDataUtil.put(cursor, "$content", digest.getKey());
 		            IDataUtil.put(cursor, "$digest", ObjectHelper.convert(digest.getValue(), mode));
@@ -70,7 +73,7 @@ public final class content
 		}
 		// --- <<IS-END>> ---
 
-
+                
 	}
 }
 
