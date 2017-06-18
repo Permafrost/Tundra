00271b0b1d80692ef89e42cca90c4462da46d669
Lachlan Dowding <lachlan@permafro.st>
2017-06-18 20:26:43 +1000

    Extract response handling code from tundra.message:receive into tundra.support.receive:respond

---
+++ code/source/tundra/support/receive.java
@@ -0,0 +1,100 @@
+package tundra.support;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2017-06-18 18:50:03 EST
+// -----( ON-HOST: 192.168.66.132
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.wm.lang.ns.NSService;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+import permafrost.tundra.data.IDataHelper;
+import permafrost.tundra.io.InputStreamHelper;
+import permafrost.tundra.lang.ExceptionHelper;
+import permafrost.tundra.server.ServiceHelper;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class receive
+
+{
+	// ---( internal utility methods )---
+
+	final static receive _instance = new receive();
+
+	static receive _newInstance() { return new receive(); }
+
+	static receive _cast(Object o) { return (receive)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void respond (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(respond)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] object:0:optional $response.content
+		// [i] field:0:optional $response.content.type
+		// [i] field:0:optional $response.content.encoding
+		// [i] object:0:optional $exception
+		// [i] field:0:optional $exception?
+		// [i] field:0:optional $exception.class
+		// [i] field:0:optional $exception.message
+		IDataCursor cursor = pipeline.getCursor();
+		
+		try {
+		    Object content = IDataHelper.getOrDefault(cursor, "$response.content", Object.class, "");
+		    String contentType = IDataHelper.get(cursor, "$response.content.type", String.class);
+		    Charset charset = IDataHelper.get(cursor, "$response.content.encoding", Charset.class);
+		    Throwable exception = IDataHelper.get(cursor, "$exception", Throwable.class);
+		
+		    int responseCode;
+		    InputStream responseBody;
+		
+		    List<NSService> stack = ServiceHelper.getCallStack();
+		    if (stack.size() > 0) stack.remove(stack.size() - 1); // remove call to this service
+		    boolean initiator = stack.size() <= 1;
+		
+		    if (initiator) {
+		        if (exception == null) {
+		            responseCode = 202;
+		            responseBody = InputStreamHelper.normalize(content, charset);
+		        } else {
+		            String exceptionClass = exception.getClass().getName();
+		            if ("permafrost.tundra.content.MalformedException".equals(exceptionClass)) {
+		                responseCode = 400;
+		            } else if ("permafrost.tundra.lang.SecurityException".equals(exceptionClass)) {
+		                responseCode = 403;
+		            } else if ("permafrost.tundra.content.UnsupportedException".equals(exceptionClass)) {
+		                responseCode = 406;
+		            } else if ("permafrost.tundra.content.DuplicateException".equals(exceptionClass)) {
+		                responseCode = 409;
+		            } else if ("permafrost.tundra.content.ValidationException".equals(exceptionClass)) {
+		                responseCode = 422;
+		            } else {
+		                responseCode = 500;
+		            }
+		            responseBody = InputStreamHelper.normalize(ExceptionHelper.getMessage(exception), charset);
+		        }
+		
+		        ServiceHelper.respond(responseCode, null, null, responseBody, contentType, charset);
+		    } else {
+		        ExceptionHelper.raise(exception);
+		    }
+		} finally {
+		    cursor.destroy();
+		}
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
