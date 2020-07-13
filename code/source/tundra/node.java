a838edcc6e7d8d9de8be651e12ed6b74c386ca4b
Lachlan Dowding <lachlan@permafrost.space>
2020-07-14 05:26:36 +1000

    Fix tundra.node:list to use a new empty array when calling toArray(T[]) method to take advantage of JVM optimizations

--- code/source/tundra/node.java
+++ code/source/tundra/node.java
@@ -1,8 +1,8 @@
 package tundra;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2017-05-07 11:30:35 EST
-// -----( ON-HOST: 192.168.66.129
+// -----( CREATED: 2020-07-14T05:25:35.560
+// -----( ON-HOST: -
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -44,18 +44,18 @@ public final class node
 		// [i] - field:0:required type {"list","read","write","execute"}
 		// [i] - field:0:optional acl
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		    String node = IDataHelper.get(cursor, "$node", String.class);
 		    IData[] permissions = IDataHelper.get(cursor, "$permissions", IData[].class);
-		
+
 		    NodeHelper.setPermissions(node, permissions);
 		} finally {
 		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -67,9 +67,9 @@ public final class node
 		// @subtype unknown
 		// @sigtype java 3.5
 		// [i] field:0:optional $node
-		// [o] field:0:required $exists?
+		// [o] field:0:required $exists? {"false","true"}
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		    String node = IDataHelper.get(cursor, "$node", String.class);
 		    IDataHelper.put(cursor, "$exists?", NodeHelper.exists(node), String.class);
@@ -78,7 +78,7 @@ public final class node
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -96,23 +96,23 @@ public final class node
 		// [o] field:1:required $nodes
 		// [o] field:0:required $nodes.length
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		    String parent = IDataHelper.get(cursor, "$interface", String.class);
 		    String pattern = IDataHelper.get(cursor, "$pattern", String.class);
 		    String type = IDataHelper.get(cursor, "$type", String.class);
 		    boolean recurse = IDataHelper.getOrDefault(cursor, "$recurse?", Boolean.class, false);
-		
+
 		    SortedSet<String> set = NodeHelper.list(parent, pattern, type, recurse);
-		
-		    IDataHelper.put(cursor, "$nodes", set.toArray(new String[set.size()]));
+
+		    IDataHelper.put(cursor, "$nodes", set.toArray(new String[0]));
 		    IDataHelper.put(cursor, "$nodes.length", set.size(), String.class);
 		} finally {
 		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -124,9 +124,9 @@ public final class node
 		// @subtype unknown
 		// @sigtype java 3.5
 		// [i] field:0:optional $node
-		// [o] field:0:optional $type
+		// [o] field:0:optional $type {"service","record","interface","Flat File Schema"}
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		    String node = IDataHelper.get(cursor, "$node", String.class);
 		    NSType type = NodeHelper.getNodeType(node);
@@ -136,7 +136,7 @@ public final class node
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 }
 
